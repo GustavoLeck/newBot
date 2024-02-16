e05cf0c60d5f8ad56d69a84c0eecbe561743bc2c
Gustavo Leck <gustavoleck@hotmail.com>
2024-02-16 20:10:06 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import Status from "./routes/status-route";
-import User from "./routes/user/user-route";
 
 const server = express();
 
 server.use(bodyParser.json());
 
 server.use("/api", Status);
-server.use("/api", User);
 
 export { server };
